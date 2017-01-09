627fb36ea332835020522b033e6fbe5001668a07
Matt Becker <matt.s.becker@gmail.com>
2017-01-08 20:57:45 -0500

    After login, be sure to redirect to the pretty_weight_entries_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery unless: -> { request.format.json? } 
     def after_sign_in_path_for(resource_or_scope)
-        user_weight_entries_path
+        pretty_weight_entries_path
     end
 
     def after_sign_out_path_for(resource_or_scope)
